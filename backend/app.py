2e27a7d10e7dc369f37d693bd368707d3f2d030f
pc-beast <impavanesh@gmail.com>
2022-01-06 14:47:07 +0530

    f

--- backend/app.py
+++ backend/app.py
@@ -180,4 +180,5 @@ def root():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
